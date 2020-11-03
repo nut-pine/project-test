a63ffdb7b2c9a3979a34e56cdfa14d51f9929a95
Vitalik Kopach <apple_air@ukr.net>
2020-11-03 12:39:04 +0200

    added method mul

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,9 +5,16 @@ public class App {
         int y = 10;
 
         System.out.println(sum(x,y));
+        System.out.println(mul(x,y));
+
     }
 
     public static int sum(int x, int y){
         return  x + y;
     }
+
+    public static int mul(int x, int y){
+        return  x * y;
+    }
+
 }
