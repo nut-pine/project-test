45b43776e7100d22ed5375c740ea04eaf3514374
Vitalik Kopach <apple_air@ukr.net>
2020-11-03 12:33:34 +0200

    added methods sum()

---
+++ src/main/java/App.java
@@ -0,0 +1,13 @@
+public class App {
+
+    public static void main(String [] args){
+        int x = 15;
+        int y = 10;
+
+        System.out.println(sum(x,y));
+    }
+
+    public static int sum(int x, int y){
+        return  x + y;
+    }
+}
